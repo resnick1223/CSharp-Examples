22046aee9347dc3ffdf98a2719ce42378af88d49
resnick1223 <resnick1223@gmail.com>
2018-06-07 22:00:24 +0800

    為BMI計算機 增加結果的判斷

--- CS297-Examples/Lecture02-Examples/Example01/MainWindow.cs
+++ CS297-Examples/Lecture02-Examples/Example01/MainWindow.cs
@@ -54,8 +54,20 @@ namespace Example01
                 MessageBox.Show("身高不能是0喔");
                 return;
             }
-
             resultLabel.Text = string.Format("BMI: {0}", bmi.ToString("#.##"));
+
+            if(bmi >= 25)
+            {
+                MessageBox.Show("這樣不行喔，要注意!");
+            }else if(bmi <= 20)
+            {
+                MessageBox.Show("太瘦囉QQ，多吃點!");
+
+            }
+            else
+            {
+                MessageBox.Show("不錯喔，繼續保持!");
+            }
         }
     }
 }
