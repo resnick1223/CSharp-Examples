2fa620fc31e7389d8bc7b12d06cdb372cea9876c
resnick1223 <resnick1223@gmail.com>
2018-08-31 18:02:49 +0800

    新增 C#進階303期 第5講

---
+++ ACS303-Examples/Lecture05/BaseballGame/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballGame
+{
+    public class Ball
+    {
+        public event EventHandler BallInPlay;
+
+        public BallStatus Status { get; set; }
+        public string Name { get; set; }
+
+        public void ChangeStatus(Random random)
+        {
+            Status = new BallStatus();
+            Status.IsHitted = random.Next(3) == 0;
+            if (Status.IsHitted)
+            {
+                Status.X = random.Next(-100, 100);
+                Status.Y = random.Next(80, 100);
+                this.BallInPlay(this, Status);
+            }
+        }
+    }
+}
