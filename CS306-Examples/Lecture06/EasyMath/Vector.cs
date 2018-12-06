493c338be3ca2500d39a012b0be233d4bd13de66
resnick1223 <resnick1223@gmail.com>
2018-12-06 22:37:48 +0800

    新增CS306 第06講內容

---
+++ CS306-Examples/Lecture06/EasyMath/Vector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMath
+{
+    public class Vector : IDisposable
+    {
+        public double x;
+        public double y;
+        public string name;
+
+        public static int Count;
+
+        public Vector()
+        {
+            Vector.Count++;
+        }
+
+        public void Dispose()
+        {
+            Vector.Count--;
+        }
+
+        #region 縮放方法
+
+        /// <summary>
+        /// 將x分量放大factor倍
+        /// </summary>
+        /// <param name="factor">縮放倍數</param>
+        public void ScaleX(double factor)
+        {
+            this.x *= factor;
+        }
+
+        /// <summary>
+        /// 將y分量放大factor倍
+        /// </summary>
+        /// <param name="factor">縮放倍數</param>
+        public void ScaleY(double factor)
+        {
+            this.y *= factor;
+        }
+
+        /// <summary>
+        /// 同時縮放向量，x分量放大factorX倍，y分量放大factorY倍
+        /// </summary>
+        /// <param name="factorX">x方向的縮放倍數</param>
+        /// <param name="factorY">y方向的縮放倍數</param>
+        public void Scale(double factorX, double factorY)
+        {
+            this.ScaleX(factorX);
+            this.ScaleY(factorY);
+        }
+
+        #endregion 縮放方法
+
+        public double GetLength()
+        {
+            return Math.Sqrt(this.x * this.x + this.y * this.y);
+        }
+
+        public void WriteLine()
+        {
+            Console.WriteLine("{2}.x = {0}, {2}.y = {1}"
+                , this.x, this.y, this.name == null ? "vector" : this.name);
+        }
+    }
+}
---
+++ CS306-Examples/Lecture06/Example04/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example04
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // 建立一個內容是 0, 1, 2, ...., 9的清單
+            List<int> numbers = new List<int>();
+            for (int index = 0; index < 10; index++)
+                numbers.Add(index);
+
+            // 建立一個結果的清單
+            List<int> results = new List<int>();
+
+            // 亂數產生器
+            Random random = new Random();
+
+            // 取出四個數字
+            for (int index2 = 1; index2 <= 4; index2++)
+            {
+                // 亂數產生要抽出的位置
+                int index = random.Next(numbers.Count);
+
+                // 根據位置 拿到數字
+                int number = numbers[index];
+
+                // 拿到後加到結果的清單
+                results.Add(number);
+
+                // 移除已經用過的數字
+                numbers.Remove(number);
+            }
+
+            foreach (var number in results)
+                Console.Write(number);
+        }
+    }
+}
